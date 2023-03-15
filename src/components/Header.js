a9900ec552530d18e9b8d14b815ea5a9468c0c53
ThanhSon02 <thanhson.hw@gmail.com>
2023-03-15 16:30:04 +0700

    update login and logout feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,50 @@
+import { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../pages/UserContext";
 
 function Header() {
+    const { userInfo, setUserInfo } = useContext(UserContext);
+    useEffect(() => {
+        fetch("http://localhost:4000/profile", {
+            credentials: "include",
+        }).then((response) => {
+            response.json().then((userInfo) => {
+                setUserInfo(userInfo);
+            });
+        });
+    }, []);
+
+    const logout = () => {
+        fetch("http://localhost:4000/logout", {
+            credentials: "include",
+            method: "POST",
+        });
+        setUserInfo(null);
+    };
+
+    const username = userInfo?.username;
+
     return (
         <header>
             <Link to="/" className="logo">
                 MyBlog
             </Link>
             <nav>
-                <Link to="/login" className="">
-                    Login
-                </Link>
-                <Link to="/register" className="">
-                    Register
-                </Link>
+                {username ? (
+                    <>
+                        <Link to={"/create"}>Create new blog</Link>
+                        <a onClick={logout}>Logout</a>
+                    </>
+                ) : (
+                    <>
+                        <Link to="/login" className="">
+                            Login
+                        </Link>
+                        <Link to="/register" className="">
+                            Register
+                        </Link>
+                    </>
+                )}
             </nav>
         </header>
     );
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,16 +1,34 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { UserContext } from "./UserContext";
+
 function LoginPage() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [redirect, setRedirect] = useState(false);
+    const { setUserInfo } = useContext(UserContext);
 
     const Login = async (e) => {
         e.preventDefault();
-        await fetch("http://localhost:4000/login", {
+        const response = await fetch("http://localhost:4000/login", {
             method: "POST",
             body: JSON.stringify({ username, password }),
             headers: { "Content-Type": "application/json" },
+            credentials: "include",
         });
+        if (response.ok) {
+            response.json().then((userInfo) => {
+                setUserInfo(userInfo);
+                setRedirect(true);
+            });
+        } else {
+            alert("Wrong credentials");
+        }
     };
+
+    if (redirect) {
+        return <Navigate to={"/"} />;
+    }
 
     return (
         <form className="login" onSubmit={Login}>
